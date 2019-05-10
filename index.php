8f3f229b7458f38f823081fa4ab7b3296ff87628
gespox <saitokan@yandex.com>
2019-05-10 21:42:04 +0300

    yapim asamasinda

--- index.php
+++ index.php
@@ -11,37 +11,13 @@
     <title>Duyuru Ekran</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="#">Duyuru Ekran</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Ana Sayfa</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Hakkimizda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Iletisim</a>
-                </li>
-                <li class="nav-item">
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Giris</button>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php require_once "navbar.php"; ?>
 <header>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"> </li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"> </li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"> </li>
         </ol>
         <div class="carousel-inner" role="listbox">
             <!-- Slide One - Set the background image for this slide in the line below -->
