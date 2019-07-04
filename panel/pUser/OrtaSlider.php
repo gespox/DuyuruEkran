e0e2138c96e116b048e07ede7b2fff4c95201b1d
gespox <saitokan@yandex.com>
2019-07-04 14:40:14 +0300

    Update OrtaSlider.php

--- panel/pUser/OrtaSlider.php
+++ panel/pUser/OrtaSlider.php
@@ -106,6 +106,37 @@ if(isset($_POST["submit"])) {
         }
     }
 }?>
+        <div class="w3-container w3-margin-top-16">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 </html>
